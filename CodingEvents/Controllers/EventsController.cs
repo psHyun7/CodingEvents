7a0eb97ae2346996751ce4418a963f44791e9a67
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-24 17:34:46 -0400

    Finished Exercise

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
-
+       
         // Get: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -27,9 +28,45 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string desc)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, desc);
+            EventData.Add(newEvent);
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(var id in eventIds)
+            {
+                EventData.Remove(id);
+            }
+
+            return Redirect("/Events");
+        }
+
+        [Route("/Events/Edit/{eventId?}")]
+        public IActionResult Edit(int eventId)
+        {
+            Event eventToEdit = EventData.GetById(eventId);
+            ViewBag.title = $"Edit Event {eventToEdit.Name} (id={eventToEdit.Id})";
+            ViewBag.eventToEdit = eventToEdit;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event eventToEdit = EventData.GetById(eventId);
+            eventToEdit.Name = name;
+            eventToEdit.Description = description;
             return Redirect("/Events");
         }
     }
