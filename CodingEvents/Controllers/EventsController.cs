25818a77e2bc9705a22b8e2a0653c4c3319e6b45
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-24 12:50:10 -0400

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        // Get: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            return Redirect("/Events");
+        }
+    }
+}
