1d9ee79bc10977117d2bd7084c5a6d2a8c8aff09
Sung Park <sung_hyun.park@outlook.com>
2020-06-29 11:10:22 -0400

    Finished Lesson 15

--- CodingEvents/Models/Event.cs
+++ CodingEvents/Models/Event.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace CodingEvents.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ContactEmail { get; set; }
+
         public int Id { get; }
         private static int nextId = 1;
 
@@ -17,10 +20,11 @@ namespace CodingEvents.Models
             Id = nextId;
             nextId++;
         }
-        public Event(string name, string description) : this()
+        public Event(string name, string description, string contactEmail) : this()
         {
             Name = name;
             Description = description;
+            ContactEmail = contactEmail;
         }
 
         public override string ToString()
