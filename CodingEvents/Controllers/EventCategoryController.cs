dfa610dfc3cd1983626c57f1a9235f13ba442ad5
Sung Park <sung_hyun.park@outlook.com>
2020-07-08 13:03:21 -0400

    Added EventCategory (Chapter19 Exercise)

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // Get: /EventCategory/Index
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategories = context.EventCategories.ToList();
+
+            return View(eventCategories);
+        }
+    }
+}
